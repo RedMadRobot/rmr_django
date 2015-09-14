a5e110b0b9c88c5a58d92863882a87352151d5f5
Rinat Khabibiev <rh@redmadrobot.com>
2015-09-14 12:43:50 +0300

    removed unnecessary api version and type from the json response

--- rmr/views/json.py
+++ rmr/views/json.py
@@ -10,13 +10,7 @@ class Json(View):
 
     http_status_code = 200
 
-    api_version = 1
-
     error_code_success = ''
-
-    error_message_success = ''
-
-    type = NotImplemented
 
     logger = logging.getLogger('backend')
 
@@ -28,15 +22,14 @@ class Json(View):
                 return result
             api_result = dict(
                 error_code=self.error_code_success,
-                error_message=self.error_message_success,
                 result=result,
             )
         except ApiError as error:
 
             self.logger.log(
                 error.level,
-                '%(type)s: %(message)s (%(code)s)',
-                dict(type=self.type, message=error.message, code=error.code),
+                '%(code)s: %(message)s',
+                dict(message=error.message, code=error.code),
             )
 
             http_status_code = error.http_status_code
@@ -45,11 +38,6 @@ class Json(View):
                 error_code=error.code,
                 result=None,
             )
-
-        api_result.update(
-            api_version=self.api_version,
-            type=self.type,
-        )
 
         return JsonResponse(api_result, status=http_status_code)
 
