f0e7bcb72ed96f15fe08e5a2a138b61989239603
Rinat Khabibiev <rh@redmadrobot.com>
2015-09-22 15:18:43 +0300

    new version counter

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as description:
 
 setup(
     name='rmr_django',
-    version='0.0.3',
+    version='1.0.0',
     author='Rinat Khabibiev',
     author_email='rh@redmadrobot.com',
     packages=[
