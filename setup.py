e5cb1655e03ef6e0425d949e8dcb8449ca280fdf
Rinat Khabibiev <rh@redmadrobot.com>
2015-10-20 18:42:44 +0300

    next version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as description:
 
 setup(
     name='rmr_django',
-    version='1.0.8',
+    version='1.0.9',
     author='Rinat Khabibiev',
     author_email='rh@redmadrobot.com',
     packages=[
