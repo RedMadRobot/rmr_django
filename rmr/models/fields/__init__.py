778f39b5c5d543d660e242483573bc4947b9009a
Rinat Khabibiev <rh@redmadrobot.com>
2015-12-03 09:36:07 +0300

    Added custom PostgreSQL range fields with `upper` and `lower` lookups

--- rmr/models/fields/__init__.py
+++ rmr/models/fields/__init__.py
@@ -1,2 +1,4 @@
 from .hash_lookup import HashLookup
 from .pg_trigram import PgTrigramCharField, PgTrigramTextField
+from .range import DateRangeField, DateTimeRangeField, FloatRangeField, \
+    BigIntegerRangeField, IntegerRangeField
