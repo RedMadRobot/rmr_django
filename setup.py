e43f5b9ec4916d1e92fe22d464509bfaf9413680
Rinat Khabibiev <rh@redmadrobot.com>
2015-12-08 09:27:17 +0300

    new version 1.0.18

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as description:
 
 setup(
     name='rmr_django',
-    version='1.0.17',
+    version='1.0.18',
     author='Rinat Khabibiev',
     author_email='rh@redmadrobot.com',
     packages=[
