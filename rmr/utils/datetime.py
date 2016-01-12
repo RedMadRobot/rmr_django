531cdc506f06d0e849b4da57b0fbee9d51234076
Rinat Khabibiev <rh@redmadrobot.com>
2016-01-12 10:52:59 +0300

    rmr.utils.datetime.strptime()

--- rmr/utils/datetime.py
+++ rmr/utils/datetime.py
@@ -5,3 +5,10 @@ from django.utils import timezone
 
 def fromtimestamp(timestamp):
     datetime.fromtimestamp(timestamp, tz=timezone.get_current_timezone())
+
+
+def strptime(date_string, format, default_tz=timezone.utc):
+    dt = datetime.strptime(date_string, format)
+    if timezone.is_naive(dt):
+        dt = timezone.make_aware(dt, timezone=default_tz)
+    return timezone.localtime(dt)
