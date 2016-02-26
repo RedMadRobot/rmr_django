e7b62954ff10d63e32d146c4307c83b3aaabc6a2
Rinat Khabibiev <srenskiy@gmail.com>
2016-02-26 15:41:27 +0300

    reworked FixCacheControlMaxAge middleware

--- rmr/extensions/middleware/cache.py
+++ rmr/extensions/middleware/cache.py
@@ -1,22 +1,23 @@
+from django.middleware.cache import UpdateCacheMiddleware
 from django.utils.cache import patch_cache_control
 from django.utils.http import parse_http_date
 from django.utils.timezone import now
 
 
-class FixCacheControlMaxAge:
+class FixCacheControlMaxAge(UpdateCacheMiddleware):
     """
     Replaces 'max-age' value of 'Cache-Control' header by value calculated
     from 'Expires' header's date
 
-    Must be placed before Django's 'FetchFromCacheMiddleware' due to fact
-    that the last one gets 'Cache-Control' header from the cache where
-    it's value corresponds to the moment original response was generated
-    and that's why maybe incorrect for the current time
+    Django's FetchFromCacheMiddleware gets 'Cache-Control' header
+    for the response from the cache where it's value corresponds
+    to the moment original response was generated and thus maybe
+    incorrect for the current time
     """
 
-    @staticmethod
-    def process_response(request, response):
-        if 'Expires' in response:
+    def process_response(self, request, response):
+        should_update_cache = self._should_update_cache(request, response)
+        if not should_update_cache and 'Expires' in response:
             expires = parse_http_date(response['Expires'])
             timeout = expires - int(now().timestamp())
             patch_cache_control(response, max_age=timeout)
